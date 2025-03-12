c3fad6c1db55cfc9b2c6a41e97e410305f3f52c6
Bruno Estevão <brunoestevaoti@gmail.com>
2025-03-12 16:31:53 -0400

    Ajustes

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -39,13 +39,8 @@ class PostController
     public function editarPost($id_post, $titulo, $descricao, $categoria_id, $autor_id)
     {
         try {
-            $resultado = $this->postModel->editarPost($id_post, $titulo, $descricao, $categoria_id, $autor_id);
 
-            if ($resultado) {
-                return "Post editado com sucesso!";
-            } else {
-                return "Falha ao editar o post.";
-            }
+            $resultado = $this->postModel->editarPost($id_post, $titulo, $descricao, $categoria_id, $autor_id);
         } catch (Exception $erro) {
             return "Erro ao editar o post: " . $erro->getMessage();
         }
